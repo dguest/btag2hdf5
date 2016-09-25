2e4b8a04dada32089cc9c7c6551510ad9313ccc3
Dan Guest <dguest@cern.ch>
2016-09-25 21:27:08 +0200

    Clean up options

--- include/Options.hh
+++ include/Options.hh
@@ -13,14 +13,7 @@ struct Options
   bool verbose;
 };
 std::ostream& operator<<(std::ostream&, const Options&);
-struct StreamOptions
-{
-  std::vector<std::string> input_files;
-  double weight;
-};
 
 Options get_opts(int argc, char* argv[], const std::string& description);
-StreamOptions get_stream_opts(int argc, char* argv[],
-                              const std::string& description);
 
 #endif
