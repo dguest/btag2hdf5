e5893bacc22a9acf15e2c8e63827f8c0254cb994
Dan Guest <dguest@cern.ch>
2016-09-26 16:35:58 +0200

    add output for setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,23 @@
 # setup script for HDF5 dumper
+echo -n "setting up ATLAS..."
 setupATLAS -q
+echo " done"
 
 # -- get boost --
+echo -n "setting up boost..."
 localSetupBoost boost-1.60.0-python2.7-x86_64-slc6-gcc49 -q
 # required by makefile
 export BOOST_PATH=$ALRB_BOOST_ROOT
+echo " done"
 
 # -- get root --
+echo -n "setting up root..."
 localSetupROOT -q
+echo " done"
 
 # -- get HDF5 --
+echo -n "setting up HDF5..."
 lsetup hdf5 -q
 export HDF_PATH=$ALRB_HDF5_PATH
+echo " done"
+
